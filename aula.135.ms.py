6ed114f0687a13218d4ccb8a87625b0bcfa20b1e
Artursandro007 <166613413+Artursandro007@users.noreply.github.com>
2024-04-11 08:33:39 -0300

    Add files via upload

---
+++ aula.135.ms.py
@@ -0,0 +1,6 @@
+# des cobrindo o desconto
+print (' bom dia ')
+preço = float (input("escreva o preço "))
+desconto = float(input("escreva seu desconto "))
+formula = preço - (preço * desconto / 100 )
+print (" o seu preço atual e de", formula,)
